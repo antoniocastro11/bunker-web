7f85e95f83edc19e7e0077d76ea8f8a4d2a04f20
castro <antoniog_castro@hotmail.com>
2026-01-27 17:43:55 -0400

    feat: começo da pagina de login

---
+++ ideabunker/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+import {Input} from "@/app/components/input";
+
+export default function LoginPage(){
+    return (<> <h1>Login Page</h1>
+        <Input variant="default" inputSize="md" state="default" />
+        </>
+     );
+}
